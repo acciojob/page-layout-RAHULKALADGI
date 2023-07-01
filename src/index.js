14d936a5ae84be1b9ac48943f4e4b545d4b00305
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-07-01 13:43:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+let header = "Welcome to my website.";
+let footer = "@ 2023 My Website. All rights reserved."
+ReactDOM.render(<App header={header} footer={footer}><p>This is the content of my website.</p></App> , document.getElementById("root"));
